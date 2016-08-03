8e555a26279f20b479fdf1f83f41e951db4e58db
nabettu <tatsuaki.w@gmail.com>
2016-08-03 13:30:51 +0900

    jsでbowerからコピーするファイルが無いのにcopyが走ってエラーになっていたの解消

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -31,15 +31,6 @@ gulp.task('sass', () => {
 
 gulp.task('css', gulp.series('sass'));
 
-
-// js
-gulp.task('copy-bower', () => {
-    const config = readConfig(`${CONFIG}/copy-bower.json`);
-    return gulp.src(config.src, {
-        cwd: 'bower_components'
-    }).pipe(gulp.dest(`${DEST}/js/lib`));
-});
-
 gulp.task('browserify', () => {
     return browserify(`${SRC}/js/script.js`)
         .transform(babelify)
@@ -49,8 +40,7 @@ gulp.task('browserify', () => {
         .pipe(gulp.dest(`${DEST}/js`));
 });
 
-gulp.task('js', gulp.parallel('browserify', 'copy-bower'));
-
+gulp.task('js', gulp.parallel('browserify'));
 
 // html
 gulp.task('pug', () => {
