81544f6ed17c056a0e113102c7cc8ff12f029778
hkitahara <kitahara-hiroaki@in.kayac.com>
2016-09-05 11:26:40 +0900

    testサーバーのコメントアウトを消去
    POSTに対応するつもりならfsも読み込むべきで、一貫性がなかった

--- testDiagnosisServer/index.js
+++ testDiagnosisServer/index.js
@@ -1,10 +1,6 @@
 var path = require('path');
 var express = require('express');
-// var bodyParser = require('body-parser');
 var app = express();
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('port', 3002);
 
