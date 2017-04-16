30378ec203645d3179a101fe693a12b795e0f849
fujisawa-shin <fujisawa-shin@kayac.com>
2017-04-17 00:37:10 +0900

    html関連の依存を落とす

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,11 +8,9 @@ import sassGlob from 'gulp-sass-glob';
 import pleeease from 'gulp-pleeease';
 import browserify from 'browserify';
 import babelify from 'babelify';
-import pug from 'gulp-pug';
 import browserSync from 'browser-sync';
 import readConfig from 'read-config';
 import watch from 'gulp-watch';
-import RevLogger from 'rev-logger';
 
 
 // const
@@ -21,11 +19,6 @@ const CONFIG = './src/config';
 const HTDOCS = './public';
 const BASE_PATH = '';
 const DEST = `${HTDOCS}${BASE_PATH}`;
-
-const revLogger = new RevLogger({
-    'style.css': `${DEST}/css/style.css`,
-    'script.js': `${DEST}/js/script.js`
-});
 
 
 // css
@@ -51,23 +44,6 @@ gulp.task('browserify', () => {
 
 gulp.task('js', gulp.parallel('browserify'));
 
-// html
-gulp.task('pug', () => {
-    const locals = readConfig(`${CONFIG}/meta.yml`);
-    locals.versions = revLogger.versions();
-    locals.basePath = BASE_PATH;
-    
-    return gulp.src(`${SRC}/pug/**/[!_]*.pug`)
-        .pipe(pug({
-            locals: locals,
-            pretty: true,
-            basedir: `${SRC}/pug`
-        }))
-        .pipe(gulp.dest(`${DEST}`));
-});
-
-gulp.task('html', gulp.series('pug'));
-
 
 // serve
 gulp.task('browser-sync', () => {
@@ -81,19 +57,11 @@ gulp.task('browser-sync', () => {
 
     watch([`${SRC}/scss/**/*.scss`], gulp.series('sass', browserSync.reload));
     watch([`${SRC}/js/**/*.js`], gulp.series('browserify', browserSync.reload));
-    watch([
-        `${SRC}/pug/**/*.pug`,
-        `${SRC}/config/meta.yml`
-    ], gulp.series('pug', browserSync.reload));
-
-    revLogger.watch((changed) => {
-        gulp.series('pug', browserSync.reload)();
-    });
 });
 
 gulp.task('serve', gulp.series('browser-sync'));
 
 
 // default
-gulp.task('build', gulp.parallel('css', 'js', 'html'));
+gulp.task('build', gulp.parallel('css', 'js'));
 gulp.task('default', gulp.series('build', 'serve'));
