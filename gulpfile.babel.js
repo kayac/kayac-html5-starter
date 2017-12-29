4fd69309f127eade1837810b7303ca56a9d6e063
nobii <fujisawa-shin@kayac.com>
2017-12-29 19:22:38 +0900

    うごかす

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -51,7 +51,7 @@ gulp.task('watchify', () => {
             gutil.log(err.message);
             gutil.log(err.codeFrame);
             this.emit('end');
-        })           
+        })
         .pipe(source('script.js'))
         .pipe(gulp.dest(`${DEST}/js`));
 });
@@ -60,10 +60,12 @@ gulp.task('js', gulp.parallel('watchify'));
 
 // html
 gulp.task('pug', () => {
-    const locals = readConfig(`${CONFIG}/meta.yml`);
-    locals.versions = revLogger.versions();
-    locals.basePath = BASE_PATH;
-    
+    const locals = {
+        meta: readConfig(`${CONFIG}/meta.yml`),
+        versions: revLogger.versions(),
+        basePath: BASE_PATH
+    };
+
     return gulp.src(`${SRC}/pug/**/[!_]*.pug`)
         .pipe(pug({
             locals: locals,
