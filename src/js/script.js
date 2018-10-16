763420d1897c7cc1a98d192660290d46d3fe09e7
nobii <fujisawa-shin@kayac.com>
2018-10-16 20:39:02 +0900

    回転もかける

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@ const el = document.querySelector('.sample-icon');
 
 let mousePos;
 let logoPos = [0, 0];
+let logoRotate = 0;
 
 function loop () {
     if (mousePos) {
@@ -11,8 +12,13 @@ function loop () {
             logoPos[0] + (mousePos[0] - logoPos[0]) * SPEED,
             logoPos[1] + (mousePos[1] - logoPos[1]) * SPEED,
         ];
+        logoRotate = 360 * Math.atan2(
+            mousePos[1] - logoPos[1],
+            mousePos[0] - logoPos[0],
+        ) / (Math.PI * 2) + 90;
         el.style.left = `${logoPos[0]}px`;
         el.style.top = `${logoPos[1]}px`;
+        el.style.transform = `rotate(${logoRotate}deg)`;
     }
     requestAnimationFrame(loop);
 }
