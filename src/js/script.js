bf8deacdbd983ac437156172d667651adaa3d032
nobii <fujisawa-shin@kayac.com>
2018-10-16 20:40:06 +0900

    初期位置をかえた

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ const SPEED = 0.1;
 const el = document.querySelector('.sample-icon');
 
 let mousePos;
-let logoPos = [0, 0];
+let logoPos = [window.innerWidth / 2, window.innerHeight / 2];
 let logoRotate = 0;
 
 function loop () {
@@ -16,10 +16,10 @@ function loop () {
             mousePos[1] - logoPos[1],
             mousePos[0] - logoPos[0],
         ) / (Math.PI * 2) + 90;
-        el.style.left = `${logoPos[0]}px`;
-        el.style.top = `${logoPos[1]}px`;
-        el.style.transform = `rotate(${logoRotate}deg)`;
     }
+    el.style.left = `${logoPos[0]}px`;
+    el.style.top = `${logoPos[1]}px`;
+    el.style.transform = `rotate(${logoRotate}deg)`;
     requestAnimationFrame(loop);
 }
 
