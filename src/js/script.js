7be150201e21726ef6bd5d00daf8404c3d4dff68
Oshane Bailey <b4.oshany@gmail.com>
2020-02-27 16:16:42 -0500

    Refacture message.

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,9 @@ import { BASE_DIR } from '../constants.yml'
 import Sample from '@/lib/Sample';
 
 const sample = new Sample({
-    name: 'Hello World'
+    name: 'World'
 });
 
 document.querySelector('.wrapper').addEventListener('click', () => {
-    console.log(`hello, ${sample.name}. Base directory is ${BASE_DIR}.`);
+    console.log(`Hello, ${sample.name}. Base directory is ${BASE_DIR}.`);
 });
